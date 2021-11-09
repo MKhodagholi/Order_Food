1b81d388d4f2a1160100b3b1b867ad992005d257
MKhodagholi <hdlikemet@gmail.com>
2021-11-10 01:11:26 +0330

    "implement" TopNav Component

--- src/components/UI/TopNav/TopNav.js
+++ src/components/UI/TopNav/TopNav.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const TopNav = () => {};
+import Order from "../Order/Order";
+
+const TopNav = () => {
+  return (
+    <div>
+      <h2>ReactMeals</h2>
+      <Order />
+    </div>
+  );
+};
 
 export default TopNav;
