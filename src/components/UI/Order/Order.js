28cb57d0bffd2dd660b534da0a8bb1c024d190b2
MKhodagholi <hdlikemet@gmail.com>
2021-11-10 01:10:25 +0330

    "add" return

--- src/components/UI/Order/Order.js
+++ src/components/UI/Order/Order.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Order = () => {
-  <div className="order">
-    <i>Sabad</i>
-    <p>Your Cart</p>
-    <span>0</span>
-  </div>;
+  return (
+    <div className="order">
+      <i>Sabad</i>
+      <p>Your Cart</p>
+      <span>0</span>
+    </div>
+  );
 };
 
 export default Order;
