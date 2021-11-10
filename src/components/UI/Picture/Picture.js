57c14b3f552da6818f476bbad96550ce2d0d97d1
MKhodagholi <hdlikemet@gmail.com>
2021-11-10 09:52:48 +0330

    "add" Picture Component

---
+++ src/components/UI/Picture/Picture.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Picture.module.css";
+
+const Picture = () => {
+  return <div className={classes.picture}></div>;
+};
+
+export default Picture;
