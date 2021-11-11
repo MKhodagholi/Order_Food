cde57f2f8bf3be29514f2081f5f0f4df392e0bab
MKhodagholi <hdlikemet@gmail.com>
2021-11-11 09:49:17 +0330

    "add" div and hr tag from MealItem Component into MealList Component

--- src/components/Meal/MealList.js
+++ src/components/Meal/MealList.js
@@ -2,20 +2,22 @@ import React from "react";
 
 import MealItem from "./Mealtem";
 import Card from "../UI/Card/Card";
-
 import classes from "./MealList.module.css";
 
 const MealList = (props) => {
   return (
     <Card className={classes["meal-list"]}>
       {props.meals.map((meal) => (
-        <MealItem
-          key={meal.id}
-          title={meal.title}
-          text={meal.text}
-          id={meal.id}
-          amount={meal.amount}
-        />
+        <div className={classes["meal-item"]} key={meal.id}>
+          <MealItem
+            key={meal.id}
+            title={meal.title}
+            text={meal.text}
+            id={meal.id}
+            amount={meal.amount}
+          />
+          <hr />
+        </div>
       ))}
     </Card>
   );
