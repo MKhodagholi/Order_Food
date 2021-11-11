e8829bf064314b1823c9ba59cec0d2638aa61545
MKhodagholi <hdlikemet@gmail.com>
2021-11-11 14:26:58 +0330

    "add" Modal Component into MealOrderList Component

--- src/components/Meal/MealOrderList.js
+++ src/components/Meal/MealOrderList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../UI/Card/Card";
+import Modal from "../UI/Modal";
 
 import MealOrderItem from "./MealOrderItem";
 
@@ -7,28 +8,30 @@ import classes from "./MealOrderList.module.css";
 
 const MealOrderList = (props) => {
   return (
-    <Card className={classes["meal-order-list"]}>
-      {props.mealOrders.map((mealOrder) => (
-        <div key={mealOrder.id}>
-          <MealOrderItem
-            title={mealOrder.title}
-            amount={mealOrder.amount}
-            count={mealOrder.count}
-            key={mealOrder.id}
-          />
-          <br />
-          <hr />
+    <Modal>
+      <Card className={classes["meal-order-list"]}>
+        {props.mealOrders.map((mealOrder) => (
+          <div key={mealOrder.id}>
+            <MealOrderItem
+              title={mealOrder.title}
+              amount={mealOrder.amount}
+              count={mealOrder.count}
+              key={mealOrder.id}
+            />
+            <br />
+            <hr />
+          </div>
+        ))}
+        <div className={classes["total-amount"]}>
+          <h2>Total Amount</h2>
+          <h2>{"$33.00"}</h2>
         </div>
-      ))}
-      <div className={classes["total-amount"]}>
-        <h2>Total Amount</h2>
-        <h2>{"$33.00"}</h2>
-      </div>
-      <div className={classes["meal-order__btn"]}>
-        <button className={classes["btn1"]}>Close</button>
-        <button className={classes["btn2"]}>Order</button>
-      </div>
-    </Card>
+        <div className={classes["meal-order__btn"]}>
+          <button className={classes["btn1"]}>Close</button>
+          <button className={classes["btn2"]}>Order</button>
+        </div>
+      </Card>
+    </Modal>
   );
 };
 
