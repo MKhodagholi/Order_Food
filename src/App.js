a032c3e2f6b124c5d001b0ec5dc878ea5f03c444
MKhodagholi <hdlikemet@gmail.com>
2021-11-11 10:02:13 +0330

    "add" Component into App file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 import React from "react";
 
+import TopNav from "./components/UI/Header/TopNav/TopNav";
+import MealSummary from "./components/Meal/MealSummary";
+import Picture from "./components/UI/Picture/Picture";
+import MealList from "./components/Meal/MealList";
+import MealOrderList from "./components/Meal/MealOrderList";
+
 const App = () => {
-  return <div>This is a good vision</div>;
+  const mealList = [
+    {
+      title: "Sushi",
+      text: "Finest fish and veggies",
+      amount: 22.99,
+      id: Math.random().toString(),
+    },
+    {
+      title: "schnitzel",
+      text: "A german specialty",
+      amount: 16.5,
+      id: Math.random().toString(),
+    },
+    {
+      title: "Barbecue Burger",
+      text: "American, raw, meaty",
+      amount: 12.99,
+      id: Math.random().toString(),
+    },
+    {
+      title: "Green Bowl",
+      text: "Healthy...and green...",
+      amount: 18.99,
+      id: Math.random().toString(),
+    },
+  ];
+
+  const mealOrders = [
+    {
+      title: "Sushi",
+      amount: 18.99,
+      count: 3,
+      id: Math.random().toString(),
+    },
+  ];
+  return (
+    <div>
+      {/* <MealOrderList mealOrders={mealOrders} /> */}
+      <TopNav />
+      <Picture />
+      <MealSummary />
+      <MealList meals={mealList} />
+    </div>
+  );
 };
 
 export default App;
