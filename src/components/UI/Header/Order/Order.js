603b3788ee970ee715277cf1aba7a7fe7685ce8a
MKhodagholi <hdlikemet@gmail.com>
2021-11-14 21:22:58 +0330

    "add" Animation Effect Bump and "add" Dynamic bag Number

--- src/components/UI/Header/Order/Order.js
+++ src/components/UI/Header/Order/Order.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-import "./Order.css";
+import classes from "./Order.module.css";
 import Button from "../../Button/Button";
+import CartContext from "../../../../context/Cart-Context";
 
 const Order = (props) => {
+  const context = useContext(CartContext);
+  const numberOfItem = context.items.reduce((currNumber, item) => {
+    return currNumber + item.count;
+  }, 0);
+  const [showBumpButton, setShowBumpButton] = useState(true);
+  useEffect(() => {
+    setShowBumpButton(true);
+    setTimeout(() => {
+      setShowBumpButton(false);
+    }, 100);
+  }, [context.totalAmount]);
+
+  const btnClasses = `${classes.order}  ${showBumpButton ? classes.bump : ""}`;
   return (
-    <Button className="order" onClick={props.onClick}>
-      <div className="icon">
+    <Button className={btnClasses} onClick={props.onClick}>
+      <div className={classes.icon}>
         <i className="fa fa-shopping-cart"></i>
       </div>
-      <div className="your-cart">
-        Your Cart <span>0</span>
+      <div className={classes["your-cart"]}>
+        Your Cart <span>{numberOfItem}</span>
       </div>
     </Button>
   );
