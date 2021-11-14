be438f85a9e7e06ad4dbaaba8629b8701c7532ff
MKhodagholi <hdlikemet@gmail.com>
2021-11-14 21:25:59 +0330

    "add" Component Into App Component and USE Them

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React, { useState } from "react";
 
-import MealContext from "./context/MealContext";
 import TopNav from "./components/UI/Header/TopNav/TopNav";
 import MealSummary from "./components/Meal/MealSummary";
 import Picture from "./components/UI/Picture/Picture";
 import MealList from "./components/Meal/MealList";
 import MealOrderList from "./components/Meal/MealOrderList";
+import CartProvider from "./context/CartProvider";
 
 const App = () => {
-  const [mealOrders, setMealOrders] = useState([]);
-  const mealList = [
-    {
-      title: "Sushi",
-      text: "Finest fish and veggies",
-      amount: 22.99,
-      id: Math.random().toString(),
-    },
-    {
-      title: "schnitzel",
-      text: "A german specialty",
-      amount: 16.5,
-      id: Math.random().toString(),
-    },
-    {
-      title: "Barbecue Burger",
-      text: "American, raw, meaty",
-      amount: 12.99,
-      id: Math.random().toString(),
-    },
-    {
-      title: "Green Bowl",
-      text: "Healthy...and green...",
-      amount: 18.99,
-      id: Math.random().toString(),
-    },
-  ];
-
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
@@ -45,37 +17,14 @@ const App = () => {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
-
-  const addMealHandler = (id, count) => {
-    const meal = mealList.find((meal) => meal.id === id);
-    const mealOrderItem = { ...meal, count };
-    console.log(mealOrderItem);
-  };
-
-  const mealOrderList = [
-    ...mealOrders,
-    {
-      title: "Sushi",
-      count: 3,
-      amount: 22.99,
-      id: Math.random().toString(),
-    },
-  ];
   return (
-    <MealContext.Provider value={{ addMeal: addMealHandler }}>
-      <div>
-        {cartIsShown && (
-          <MealOrderList
-            mealOrders={mealOrderList}
-            onHideCart={hideCartHandler}
-          />
-        )}
-        <TopNav onShowCart={showCartHandler} />
-        <Picture />
-        <MealSummary />
-        <MealList meals={mealList} />
-      </div>
-    </MealContext.Provider>
+    <CartProvider>
+      {cartIsShown && <MealOrderList onHideCart={hideCartHandler} />}
+      <TopNav onShowCart={showCartHandler} />
+      <Picture />
+      <MealSummary />
+      <MealList />
+    </CartProvider>
   );
 };
 
