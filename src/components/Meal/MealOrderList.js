2ec5722e7e5bf5ed267ef324eb730cb9b78147d2
MKhodagholi <hdlikemet@gmail.com>
2021-11-14 21:20:53 +0330

    "add" make MealOrder list Dynamic by using UseContext

--- src/components/Meal/MealOrderList.js
+++ src/components/Meal/MealOrderList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../context/Cart-Context";
 import Card from "../UI/Card/Card";
 import Modal from "../UI/Modal";
 
@@ -7,30 +8,39 @@ import MealOrderItem from "./MealOrderItem";
 import classes from "./MealOrderList.module.css";
 
 const MealOrderList = (props) => {
+  const context = useContext(CartContext);
+  const mealOrders = context.items;
+  const totalAmount = context.totalAmount.toFixed(2);
+
   return (
     <Modal onHideCart={props.onHideCart}>
       <Card className={classes["meal-order-list"]}>
-        {props.mealOrders.map((mealOrder) => (
-          <div key={mealOrder.id}>
-            <MealOrderItem
-              title={mealOrder.title}
-              amount={mealOrder.amount}
-              count={mealOrder.count}
-              key={mealOrder.id}
-            />
-            <br />
-            <hr />
+        <div>
+          {mealOrders.map((mealOrder) => (
+            <div key={mealOrder.id}>
+              <MealOrderItem
+                title={mealOrder.title}
+                id={mealOrder.id}
+                amount={mealOrder.amount}
+                count={mealOrder.count}
+                key={mealOrder.id}
+              />
+              <br />
+              <hr />
+            </div>
+          ))}
+          <div className={classes["total-amount"]}>
+            <h2>Total Amount</h2>
+            <h2>{totalAmount}</h2>
           </div>
-        ))}
-        <div className={classes["total-amount"]}>
-          <h2>Total Amount</h2>
-          <h2>{"$33.00"}</h2>
-        </div>
-        <div className={classes["meal-order__btn"]}>
-          <button className={classes["btn1"]} onClick={props.onHideCart}>
-            Close
-          </button>
-          <button className={classes["btn2"]}>Order</button>
+          <div className={classes["meal-order__btn"]}>
+            <button className={classes["btn1"]} onClick={props.onHideCart}>
+              Close
+            </button>
+            {mealOrders.length > 0 && (
+              <button className={classes["btn2"]}>Order</button>
+            )}
+          </div>
         </div>
       </Card>
     </Modal>
