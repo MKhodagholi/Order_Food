ec61cad553af68a078b23184f2b0ed75b19f8829
MKhodagholi <hdlikemet@gmail.com>
2021-11-14 21:23:54 +0330

    "add" addItem Logic

--- src/components/Meal/Mealtem.js
+++ src/components/Meal/Mealtem.js
@@ -3,10 +3,19 @@ import React, { Fragment, useContext } from "react";
 import MealDescription from "./MealDescription";
 import MealSold from "./MealSold";
 import classes from "./MealItem.module.css";
-import MealContext from "../../context/MealContext";
+import CartContext from "../../context/Cart-Context";
 
 const MealItem = (props) => {
-  const mealContext = useContext(MealContext);
+  const context = useContext(CartContext);
+  const addToCartHandler = (count) => {
+    context.addItem({
+      id: props.id,
+      amount: props.amount,
+      title: props.title,
+      count: count,
+    });
+  };
+
   return (
     <Fragment>
       <div className={classes["meal-item__content"]}>
@@ -15,9 +24,7 @@ const MealItem = (props) => {
           text={props.text}
           amount={props.amount}
         />
-        <MealSold
-          onMealAdd={() => mealContext.addMeal(props.id, props.count || 2)}
-        />
+        <MealSold onAddItem={addToCartHandler} />
       </div>
     </Fragment>
   );
