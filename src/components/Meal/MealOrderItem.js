072f16a370b7b4da1432345e801afc6eda55279e
MKhodagholi <hdlikemet@gmail.com>
2021-11-14 21:19:38 +0330

    "add" TotalAmount Value Dynamic and "add" Increase and Decrease button Logic

--- src/components/Meal/MealOrderItem.js
+++ src/components/Meal/MealOrderItem.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../context/Cart-Context";
 
 import classes from "./MealOrderItem.module.css";
 
 const MealOrderItem = (props) => {
+  const context = useContext(CartContext);
+  const cartItemRemoveHandler = () => {
+    context.removeItem({
+      title: props.title,
+      amount: props.amount,
+      count: 1,
+    });
+  };
+  const cartItemAddHandler = () => {
+    context.addItem({
+      title: props.title,
+      id: props.id,
+      amount: props.amount,
+      count: 1,
+    });
+  };
   return (
     <div className={classes["meal-order-item"]}>
       <div className={classes["meal-order-item__content"]}>
@@ -13,8 +30,8 @@ const MealOrderItem = (props) => {
         </div>
       </div>
       <div className={classes["meal-order-item__btn"]}>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={cartItemRemoveHandler}>-</button>
+        <button onClick={cartItemAddHandler}>+</button>
       </div>
     </div>
   );
