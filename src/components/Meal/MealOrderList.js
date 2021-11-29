658a41ce217e0d6cdfebbde0b453c27179ad263b
Mkhodagholi <hdlikemet@gmail.com>
2021-11-29 09:39:45 +0330

    "add" fetch api for get order food from back-end

--- src/components/Meal/MealOrderList.js
+++ src/components/Meal/MealOrderList.js
@@ -1,16 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../context/Cart-Context";
 import Card from "../UI/Card/Card";
 import Modal from "../UI/Modal";
 
 import MealOrderItem from "./MealOrderItem";
+import useHttp from "../../hooks/use-http";
+import Form from "../Form/Form";
 
 import classes from "./MealOrderList.module.css";
 
 const MealOrderList = (props) => {
   const context = useContext(CartContext);
+  const { isLoading, error, sendRequest } = useHttp();
   const mealOrders = context.items;
   const totalAmount = context.totalAmount.toFixed(2);
+  const [isOrder, setIsOrder] = useState(false);
+
+  const orderSubmissionHandler = () => {
+    mealOrders.forEach((meal) => {
+      sendRequest({
+        url: "somethingInTheWay",
+        method: "POST",
+        body: {
+          name: meal.title,
+          id: meal.id,
+          amount: meal.amount,
+          count: meal.count,
+        },
+      });
+      setIsOrder(true);
+    });
+  };
 
   return (
     <Modal onHideCart={props.onHideCart}>
@@ -33,12 +53,20 @@ const MealOrderList = (props) => {
             <h2>Total Amount</h2>
             <h2>{totalAmount}</h2>
           </div>
+          {isOrder && <Form onHideCart={props.onHideCart} />}
           <div className={classes["meal-order__btn"]}>
-            <button className={classes["btn1"]} onClick={props.onHideCart}>
-              Close
-            </button>
-            {mealOrders.length > 0 && (
-              <button className={classes["btn2"]}>Order</button>
+            {!isOrder && (
+              <button className={classes["btn1"]} onClick={props.onHideCart}>
+                Close
+              </button>
+            )}
+            {mealOrders.length > 0 && !isOrder && (
+              <button
+                className={classes["btn2"]}
+                onClick={orderSubmissionHandler}
+              >
+                Order
+              </button>
             )}
           </div>
         </div>
